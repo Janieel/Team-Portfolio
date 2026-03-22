5b5cd817482bad8b7cea70b35bc28f657c03bfb8
miracle jane <miracleokon2003@gmail.com>
2026-03-22 08:01:05 +0100

    pushed

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* your existing config options */
+  webpack: (config) => {
+    // Add alias @ => src
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    return config;
+  },
 };
 
 export default nextConfig;
