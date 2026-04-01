073c5e883388bcc7d457d2555cf56d0d14e4858c
miracle jane <miracleokon2003@gmail.com>
2026-04-01 17:34:30 +0100

    project edit

--- src/Component/Projcase/Projcase.tsx
+++ src/Component/Projcase/Projcase.tsx
@@ -6,12 +6,12 @@ const Projcase = () => {
     <div>
         <div className=' h-20  flex justify-center items-center '>
             <div className='flex gap-6'>
-              <span className='text-[3rem] font-serif text-[#a09af8]'>Project</span>
-              <span className='text-[3rem] font-serif text-[#a09af8]'>Showcase</span>
+              <span className=' text-[2rem]  sm:text-[3rem] font-serif text-[#a09af8]'>Project</span>
+              <span className=' text-[2rem] sm:text-[3rem] font-serif text-[#a09af8]'>Showcase</span>
             </div>
         </div>
-        <div className='bg-[#473442] mt-5 flex justify-center items-center text-[#dedde9]'>
-           <span>Building products and growing communities here's what we've been working on.</span> 
+        <div className='bg-[#473442] sm:mt-5 flex justify-center items-center text-[#dedde9]'>
+           <span className='text-center px-5 text-[0.8rem] sm:text-[1rem] '>Building products and growing communities here's what we've been working on.</span> 
         </div>
         <div className=' px-5 sm:px-10 '>
             <Pov/>
